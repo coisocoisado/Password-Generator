c5c8d3bf4aa0f7a343ff9b9894d5588c8e7eb0e3
coisocoisado <brenorodrigo007@gmail.com>
2024-03-19 22:49:17 -0300

    add checkmarks and animation to the refresh button

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const pwdLength = document.getElementById("psw-length")
-const slider = document.getElementById("slider")
+const pwdLength = document.getElementById("psw-length");
+const slider = document.getElementById("slider");
 
 // make the span the current value so it is not empty
-pwdLength.innerHTML = slider.value
+pwdLength.innerHTML = slider.value;
 
 // when the slider changes its value then update the span
 slider.oninput = function() {
-    pwdLength.innerHTML = slider.value
+    pwdLength.innerHTML = slider.value;
 }
+
+const refresh = document.getElementById("refresh");
+
+refresh.addEventListener('mouseup', function() {
+    this.classList.remove('rotate');
+    setTimeout(() => {
+        this.classList.add('rotate')
+    }, 10);
+})
